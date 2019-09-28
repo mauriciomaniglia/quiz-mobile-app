a6142d5b5c3c61dbf2b06fc7fa3abae7d7ac825e
Maurício Maniglia <manigliamauricio@gmail.com>
2019-09-28 18:27:23 -0300

    Create the QuestionItem model

---
+++ QuizMobileApp/QuizMobileApp/QuestionItem.swift
@@ -0,0 +1,15 @@
+//
+//  QuestionItem.swift
+//  QuizMobileApp
+//
+//  Created by Mauricio Cesar Maniglia Junior on 28/09/19.
+//  Copyright © 2019 Mauricio Cesar Maniglia Junior. All rights reserved.
+//
+
+import Foundation
+
+struct QuestionItem {
+    let id: UUID
+    let question: String
+    let answer: [String]
+}
