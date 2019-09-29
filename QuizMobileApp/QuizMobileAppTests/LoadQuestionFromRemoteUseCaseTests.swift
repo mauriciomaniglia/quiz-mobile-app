c2c9b20ab93a5fe5fb4ec05bfed3ae734949446d
Maurício Maniglia <manigliamauricio@gmail.com>
2019-09-28 21:13:30 -0300

    Test `load` delivers error when request from URL fails.

--- QuizMobileApp/QuizMobileAppTests/LoadQuestionFromRemoteUseCaseTests.swift
+++ QuizMobileApp/QuizMobileAppTests/LoadQuestionFromRemoteUseCaseTests.swift
@@ -17,16 +17,24 @@ class RemoteQuestionLoader {
         self.store = store
     }
     
-    func load() {
-        store.get(from: url)
+    func load(completion: @escaping (Error) -> Void) {
+        store.get(from: url) { error in
+            completion(error)
+        }
     }
 }
 
 class HTTPClientSpy {
     var requestedURLs = [URL]()
+    var messages = [(Error) -> Void]()
     
-    func get(from url: URL) {
+    func get(from url: URL, completion: @escaping (Error) -> Void) {
+        messages.append(completion)
         requestedURLs.append(url)
+    }
+    
+    func complete(with error: Error, at index: Int = 0) {
+        messages[index](error)
     }
 }
 
@@ -42,7 +50,7 @@ class LoadQuestionFromRemoteUseCaseTests: XCTestCase {
         let url = URL(fileURLWithPath: "http://a-given-http-url.com")
         let (sut, store) = makeSUT(url: url)
         
-        sut.load()
+        sut.load { _ in }
         
         XCTAssertEqual(store.requestedURLs, [url])
     }
@@ -51,10 +59,24 @@ class LoadQuestionFromRemoteUseCaseTests: XCTestCase {
         let url = URL(fileURLWithPath: "http://a-given-http-url.com")
         let (sut, store) = makeSUT(url: url)
         
-        sut.load()
-        sut.load()
+        sut.load { _ in }
+        sut.load { _ in }
         
         XCTAssertEqual(store.requestedURLs, [url, url])
+    }
+    
+    func test_load_deliversErrorOnClientError() {
+        let url = URL(fileURLWithPath: "http://a-given-http-url.com")
+        let (sut, store) = makeSUT(url: url)
+        let clientError = NSError(domain: "Test", code: 0, userInfo: nil)
+        
+        var receivedError: Error?
+        sut.load { error in
+            receivedError = error
+        }
+        store.complete(with: clientError)
+        
+        XCTAssertEqual(receivedError as NSError?, clientError)
     }
     
     // MARK: - Helpers
