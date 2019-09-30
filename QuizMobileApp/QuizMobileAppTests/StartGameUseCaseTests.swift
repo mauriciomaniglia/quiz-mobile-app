32bdc2bf6c596fe9eb4f04313192f2d0d9c5b055
Maurício Maniglia <manigliamauricio@gmail.com>
2019-09-30 12:54:34 -0300

    Guarantee `startGame` delivers counter start message when seconds is greater than zero.

--- QuizMobileApp/QuizMobileAppTests/StartGameUseCaseTests.swift
+++ QuizMobileApp/QuizMobileAppTests/StartGameUseCaseTests.swift
@@ -11,19 +11,33 @@ import XCTest
 class StartGameUseCaseTests: XCTestCase {
 
     func test_init_doesNotRequestToStartTheCounter() {
-        let counter = CounterSpy()
+        let counter = CounterSpy(seconds: 1)
         _ = QuizGameEngine(counter: counter)
         
         XCTAssertEqual(counter.startCounterCallCount, 0)
     }
     
     func test_startGame_requestToStartTheCounter() {
-        let counter = CounterSpy()
+        let counter = CounterSpy(seconds: 1)
         let sut = QuizGameEngine(counter: counter)
         
-        sut.startGame()
+        sut.startGame { }
         
         XCTAssertEqual(counter.startCounterCallCount, 1)
+    }
+    
+    func test_startGame_deliversCounterStartMessageWhenSecondsIsGreaterThanZero() {
+        let counter = CounterSpy(seconds: 1)
+        let sut = QuizGameEngine(counter: counter)
+        
+        var counterStartMessage = 0
+        sut.startGame {
+            counterStartMessage += 1
+        }
+        
+        counter.startGameMessage()
+        
+        XCTAssertEqual(counterStartMessage, 1)
     }
 
 }
@@ -35,15 +49,26 @@ class QuizGameEngine {
         self.counter = counter
     }
     
-    func startGame() {
-        self.counter.start()
+    func startGame(completion: @escaping () -> Void) {
+        self.counter.start(completion: completion)
     }
 }
 
 class CounterSpy {
     var startCounterCallCount = 0
+    var seconds = 0
+    var messages = [() -> Void]()
     
-    func start() {
+    init(seconds: Int) {
+        self.seconds = seconds
+    }
+    
+    func start(completion: @escaping () -> Void) {
         startCounterCallCount += 1
+        messages.append(completion)
+    }
+    
+    func startGameMessage(at index: Int = 0) {
+        messages[index]()
     }
 }
