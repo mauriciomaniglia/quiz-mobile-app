a7c52319e770a15e0ac200bcd0ec867cec0049cf
Maurício Maniglia <manigliamauricio@gmail.com>
2019-09-30 19:17:53 -0300

    Guarantee `QuizGameEngine` do not trigger to insert answer upon creation

---
+++ QuizMobileApp/QuizMobileAppTests/Game Engine/AddAnswerUseCaseTests.swift
@@ -0,0 +1,32 @@
+//
+//  AddAnswerUseCaseTests.swift
+//  QuizMobileAppTests
+//
+//  Created by Mauricio Cesar Maniglia Junior on 30/09/19.
+//  Copyright © 2019 Mauricio Cesar Maniglia Junior. All rights reserved.
+//
+
+import XCTest
+import QuizMobileApp
+
+class AddAnswerUseCaseTests: XCTestCase {
+
+    func test_init_doesNotRequestToAddAnswer() {
+        let (_, counter) = makeSUT()
+        
+        XCTAssertEqual(counter.messages.count, 0)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: QuizGameEngine, counter: CounterSpy) {
+        let counter = CounterSpy(seconds: 1)
+        let sut = QuizGameEngine(counter: counter)
+        
+        trackForMemoryLeak(sut, file: file, line: line)
+        trackForMemoryLeak(counter, file: file, line: line)
+        
+        return (sut, counter)
+    }
+
+}
