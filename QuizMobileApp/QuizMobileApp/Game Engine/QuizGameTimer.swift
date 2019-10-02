244a6cf28190308ceff5a671c12f21332a90966e
Maurício Maniglia <manigliamauricio@gmail.com>
2019-10-02 14:56:59 -0300

    Create `QuizGameTimer` concrete implementation of `Counter`

---
+++ QuizMobileApp/QuizMobileApp/Game Engine/QuizGameTimer.swift
@@ -0,0 +1,45 @@
+//
+//  QuizGameTimer.swift
+//  QuizMobileApp
+//
+//  Created by Mauricio Cesar Maniglia Junior on 02/10/19.
+//  Copyright © 2019 Mauricio Cesar Maniglia Junior. All rights reserved.
+//
+
+import Foundation
+
+public final class QuizGameTimer: Counter {
+    private var timer: Timer?
+    private var seconds: Int
+    private var secondsReseted: Int
+    private var completion: ((CounterResult) -> Void)?
+    
+    public init(withSeconds seconds: Int) {
+        self.seconds = seconds
+        self.secondsReseted = seconds
+    }
+    
+    public func start(completion: @escaping (CounterResult) -> Void) {
+        self.completion = completion
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(QuizGameTimer.updateSeconds)), userInfo: nil, repeats: true)
+    }
+    
+    public func reset() {
+        timer?.invalidate()
+        seconds = secondsReseted
+        completion?(.reset)
+    }
+    
+    public func stop() {
+        completion?(.stop)
+    }
+    
+    @objc private func updateSeconds() {
+        if seconds > 0 {
+            completion?(.currentSecond(seconds))
+            seconds -= 1
+        } else {
+            completion?(.stop)
+        }
+    }
+}
