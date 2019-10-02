7e23b04f0d5ad9ead4ea619ba38b6c5cabf46ce5
Maurício Maniglia <manigliamauricio@gmail.com>
2019-10-02 10:56:37 -0300

    Add `QuizMobileAppiOS` application (prod and test) targets for iOS platform-specific components

---
+++ QuizMobileApp/QuizMobileAppiOS/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  QuizMobileAppiOS
+//
+//  Created by Mauricio Cesar Maniglia Junior on 02/10/19.
+//  Copyright © 2019 Mauricio Cesar Maniglia Junior. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
