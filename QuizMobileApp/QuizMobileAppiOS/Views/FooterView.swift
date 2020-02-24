2559c23ff5d67d9c086a7aff3dab8695bcd3cd26
mauriciomaniglia <manigliamauricio@gmail.com>
2020-02-24 16:57:20 -0300

    Fix animation constraint to update only the footer view when the keyboard appear/disappear

--- QuizMobileApp/QuizMobileAppiOS/Views/FooterView.swift
+++ QuizMobileApp/QuizMobileAppiOS/Views/FooterView.swift
@@ -9,21 +9,53 @@
 import UIKit
 
 final class FooterView: UIView {
-    override func updateConstraints() {
-        super.updateConstraints()
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(FooterView.keyboardWillChangeFrame), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(FooterView.keyboardWillChangeFrame), name: UIResponder.keyboardWillHideNotification, object: nil)
-         NotificationCenter.default.addObserver(self, selector: #selector(FooterView.keyboardWillChangeFrame), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    @IBOutlet private var buttonBottomConstraint: NSLayoutConstraint!
+    @IBOutlet private var footerBottomConstraint: NSLayoutConstraint!
+    
+    private var buttonBottomInitialConstraint = CGFloat.init()
+    
+    override func draw(_ rect: CGRect) {
+        saveButtonBottomConstraintInitialValue()
+        registerKeyboardObservers()
     }
     
+    deinit {
+        unregisterKeyboardObservers()
+    }
+
     @objc private func keyboardWillChangeFrame(notification:NSNotification) {
+        animateFooterForKeyboardNotification(notification)
+    }
+    
+    private func saveButtonBottomConstraintInitialValue() {
+        buttonBottomInitialConstraint = buttonBottomConstraint.constant
+    }
+    
+    private func registerKeyboardObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(FooterView.keyboardWillChangeFrame), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(FooterView.keyboardWillChangeFrame), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(FooterView.keyboardWillChangeFrame), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    private func unregisterKeyboardObservers() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    private func animateFooterForKeyboardNotification(_ notification: NSNotification) {
         guard let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
-                
-        if notification.name == UIResponder.keyboardWillShowNotification {
-            superview?.frame.origin.y = -keyboardFrame.cgRectValue.height
-        } else {
-            superview?.frame.origin.y = 0
+        superview?.layoutIfNeeded()
+        
+        UIView.animate(withDuration: 1.0) {
+            if notification.name == UIResponder.keyboardWillShowNotification {
+                self.buttonBottomConstraint.constant = keyboardFrame.cgRectValue.height + self.buttonBottomInitialConstraint
+                self.footerBottomConstraint.constant = keyboardFrame.cgRectValue.height
+            } else {
+                self.buttonBottomConstraint.constant = self.buttonBottomInitialConstraint
+                self.footerBottomConstraint.constant = 0
+            }
+            self.superview?.layoutIfNeeded()
         }
     }
 }
