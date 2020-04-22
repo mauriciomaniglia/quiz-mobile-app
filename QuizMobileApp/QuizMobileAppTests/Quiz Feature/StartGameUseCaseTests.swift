497e07758534f91a783d7b7a3f4f0a0e01f6f452
mauriciomaniglia <manigliamauricio@gmail.com>
2020-04-22 06:25:00 -0300

    Move `ResetGameUseCase` to `StartGameUseCase` because the use case for reset was removed (this is not a business rule)

--- QuizMobileApp/QuizMobileAppTests/Quiz Feature/StartGameUseCaseTests.swift
+++ QuizMobileApp/QuizMobileAppTests/Quiz Feature/StartGameUseCaseTests.swift
@@ -50,6 +50,67 @@ class StartGameUseCaseTests: XCTestCase {
         XCTAssertEqual(delegate.gameStatus?.userHitAllAnswers, false)
     }
     
+    func test_init_doesNotResetCounter() {
+        let (_, counter) = makeSUT()
+        
+        XCTAssertEqual(counter.resetCallsCount, 0)
+    }
+    
+    func test_reset_callsResetCounter() {
+        let (sut, counter) = makeSUT()
+        
+        sut.reset()
+        
+        XCTAssertEqual(counter.resetCallsCount, 1)
+    }
+    
+    func test_resetTwice_callsResetCounterTwice() {
+        let (sut, counter) = makeSUT()
+        
+        sut.reset()
+        sut.reset()
+        
+        XCTAssertEqual(counter.resetCallsCount, 2)
+    }
+    
+    func test_reset_deliversCounterReseted() {
+        let sut = QuizGameTimer(withSeconds: 1)
+        let delegate = GameDelegateSpy()
+        sut.delegate = delegate
+        
+        sut.reset()
+        
+        XCTAssertEqual(delegate.counterResetedCalls, 1)
+    }
+    
+    func test_counterReseted_deliversGameOnResetState() {
+        let (sut, _) = makeSUT()
+        let delegate = GameDelegateSpy()
+        sut.delegate = delegate
+        
+        sut.counterReseted(1)
+        
+        XCTAssertNotNil(delegate.gameStatus)
+        XCTAssertEqual(delegate.gameStatus?.isGameStarted, false)
+        XCTAssertEqual(delegate.gameStatus?.isGameFinished, false)
+        XCTAssertEqual(delegate.gameStatus?.correctAnswers, ["Answer1"])
+        XCTAssertEqual(delegate.gameStatus?.userAnswers, [])
+        XCTAssertEqual(delegate.gameStatus?.userHitAllAnswers, false)
+    }
+    
+    func test_reset_withTwoUserAnswers_deliversGameStateWithEmptyUserAnswers() {
+        let (sut, _) = makeSUT()
+        let delegate = GameDelegateSpy()
+        sut.delegate = delegate
+                
+        sut.addAnswer("Answer1")
+        sut.addAnswer("Answer2")
+        sut.reset()
+        sut.counterReseted(1)
+                
+        XCTAssertEqual(delegate.gameStatus?.userAnswers, [])
+    }
+    
     // MARK - Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: QuizGameEngine, counter: CounterSpy) {
